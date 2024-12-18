54c2698c5842210a671d114a478b34da930279b4
deepsahilz <deepsahil.online@gmail.com>
2024-12-18 21:02:42 +0530

    imports added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-
-  import Poll from './components/poll'
+import Nav from './components/nav'
+import Poll from './components/poll'
 import Profile from './components/profile'
+import HeroSection from './components/heroSection'
 function App() {
 
   return (
